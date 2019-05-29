916da790fcdbc33d5c40b88583ca25a9ead4f248
fuyuan.jia <fuyuan.jia@CNfuyuan.jia>
2019-05-29 20:28:10 +0800

    fisrtcommit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//这是程序的入口文件
+// import React from 'react'
+// import ReactDom from 'react-dom'
+// import './index.css'
+// import App from './app.js'
+// const App = require('./app');
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                helloya
+            </div>
+        )
+    }
+}
+// export default App
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
